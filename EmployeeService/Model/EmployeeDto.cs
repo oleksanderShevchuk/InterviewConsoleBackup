90bb430c405770cbd20974d64ca0ac87c4561701
OleksandrShevchukDev5 <dev5@kpp-centr.com.ua>
2025-10-28 14:28:50 +0200

    Added EmployeeDto model

---
+++ EmployeeService/Model/EmployeeDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService.Model
+{
+    public class EmployeeDto
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int? ManagerID { get; set; }
+        public bool Enable { get; set; }
+
+        public List<EmployeeDto> Employees { get; set; } = new List<EmployeeDto>();
+    }
+}
